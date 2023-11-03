4534b20287af03409253c47f83643331da317836
Vladimiros Makris <vladimiros.makris@microsoft.wsei.edu.pl>
2023-11-03 15:51:00 +0100

    Add files via upload

---
+++ MLASERP - Laser Phones.cs
@@ -0,0 +1,264 @@
+﻿using System;
+
+namespace Program
+
+{
+    class Program
+
+    {
+        enum Direction
+
+        {
+            N,
+            S,
+            W,
+            E,
+            START,
+        }
+
+        class PlotTile
+
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+
+            public int cost { get; set; }
+            public int distance { get; set; }
+
+            public int cost_distance => this.cost + this.distance;
+
+            public PlotTile? Parent { get; set; }
+
+            public void SetDistance(int targetX, int targetY)
+
+            {
+                this.distance = Math.Abs(targetX - this.X) + Math.Abs(targetY - this.Y);
+            }
+
+            public string getID()
+
+            {
+                return this.X + "." + this.Y;
+            }
+
+            public Direction direction { get; set; } = Direction.START;
+
+            public int mirror_count { get; set; } = 0;
+        }
+
+        static void TileAdd(int x, int y, string[] plot, PlotTile currentTile, List<PlotTile> possibleTiles, Direction direction, PlotTile end)
+
+        {
+            char plotChar = plot[y][x];
+            if (plotChar != '*')
+
+            {
+                int costModificator = 0;
+                int mirror = 0;
+                if (direction != currentTile.direction && currentTile.direction != Direction.START)
+
+                {
+                    mirror = 1;
+                }
+
+                var tile = new PlotTile { X = x, Y = y, Parent = currentTile, cost = currentTile.cost + 1 + costModificator, direction = direction, mirror_count = currentTile.mirror_count + mirror };
+
+                tile.SetDistance(end.X, end.Y);
+                possibleTiles.Add(tile);
+            }
+        }
+
+        static List<PlotTile> GetWalkableTiles(string[] plot, int maxX, int maxY, PlotTile currentTile, PlotTile end)
+
+        {
+            var possibleTiles = new List<PlotTile>();
+
+            int incX = currentTile.X + 1;
+            int incY = currentTile.Y + 1;
+
+            int decX = currentTile.X - 1;
+            int decY = currentTile.Y - 1;
+
+
+            if (incX < maxX)
+
+            {
+                Direction direction = Direction.E;
+                TileAdd(incX, currentTile.Y, plot, currentTile, possibleTiles, direction, end);
+            }
+
+            if (decX >= 0)
+
+            {
+                Direction direction = Direction.W;
+                TileAdd(decX, currentTile.Y, plot, currentTile, possibleTiles, direction, end);
+            }
+
+            if (incY < maxY)
+
+            {
+                Direction direction = Direction.N;
+                TileAdd(currentTile.X, incY, plot, currentTile, possibleTiles, direction, end);
+            }
+
+            if (decY >= 0)
+
+            {
+                Direction direction = Direction.S;
+                TileAdd(currentTile.X, decY, plot, currentTile, possibleTiles, direction, end);
+            }
+
+            return possibleTiles;
+        }
+
+        static void PrintPath(PlotTile endTile)
+
+        {
+            Console.WriteLine("Pos: X: " + endTile.X + ", Y: " + endTile.Y + " MI: " + endTile.mirror_count);
+            if (endTile.Parent != null)
+
+            {
+                PrintPath(endTile.Parent);
+            }
+
+        }
+
+        static PlotTile? performPathTrace(PlotTile start, PlotTile end, string[] plot, int w, int h)
+
+        {
+            start.SetDistance(end.X, end.Y);
+
+
+            List<PlotTile> activeTiles = new List<PlotTile>();
+            PriorityQueue<PlotTile, int> queue = new PriorityQueue<PlotTile, int>();
+            HashSet<string> visitedTiles = new HashSet<string>();
+
+            queue.Enqueue(start, start.mirror_count);
+
+            while (queue.Count > 0)
+
+            {
+                var currentTile = queue.Dequeue();
+
+
+                if (currentTile.X == end.X && currentTile.Y == end.Y)
+
+                {
+                    return currentTile;
+                }
+
+                if (visitedTiles.Contains(currentTile.getID()))
+
+                    continue;
+
+                else
+
+                    visitedTiles.Add(currentTile.getID());
+
+
+                var walkableTiles = GetWalkableTiles(plot, w, h, currentTile, end);
+
+                for (int i = 0; i < walkableTiles.Count(); i++)
+
+                {
+
+                    var tile = walkableTiles[i];
+                    if (!visitedTiles.Contains(tile.getID()))
+
+                    {
+                        queue.Enqueue(tile, tile.mirror_count);
+                    };
+
+
+                }
+            }
+
+            return null;
+        }
+
+        static void Main(string[] args)
+        {
+
+            var paramLine = Console.ReadLine();
+
+            if (paramLine == null)
+
+            {
+                return;
+            }
+
+            string[] par = paramLine.Split(" ");
+
+
+            int w = int.Parse(par[0]);
+            int h = int.Parse(par[1]);
+
+            if (w <= 0 || h <= 0 || w > 100 || h > 100)
+
+            {
+                return;
+            }
+
+
+            string[] plot = new string[h];
+
+            int flag = 0;
+
+            PlotTile start = new PlotTile();
+            PlotTile end = new PlotTile();
+
+
+            for (int i = 0; i < h; i++)
+
+            {
+                var plotline = Console.ReadLine();
+                if (plotline == null)
+                {
+                    return;
+                }
+                plot[i] = plotline;
+
+
+                for (int j = 0; j < plotline.Length; j++)
+
+                {
+                    char ch = plotline[j];
+                    if (ch == 'C' && flag != 2)
+
+                    {
+                        if (flag == 0)
+                        {
+                            start.X = j;
+                            start.Y = i;
+                            flag++;
+                        }
+                        else
+                        {
+                            end.X = j;
+                            end.Y = i;
+                            flag++;
+                        }
+                    }
+                }
+            }
+
+            var trace_one = performPathTrace(start, end, plot, w, h);
+            var trace_two = performPathTrace(end, start, plot, w, h);
+            if (trace_one != null && trace_two != null)
+
+            {
+                if (trace_one.mirror_count > trace_two.mirror_count)
+
+                {
+                    Console.WriteLine(trace_two.mirror_count);
+                }
+
+                else
+
+                {
+                    Console.WriteLine(trace_one.mirror_count);
+                }
+            }
+        }
+    }
+}
